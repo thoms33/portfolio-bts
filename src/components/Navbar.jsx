b94204a60d54cd349c6367c3b464bb944b496f3f
thoms33 <thomsguyot@gmail.com>
2025-03-14 02:53:38 +0100

    test de correction recursive 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/NavBar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <ul>
-        <li><Link to="/">Accueil</Link></li>
-        <li><Link to="/profile">Mon Profil</Link></li>
-        <li><Link to="/veille">Veille Technologique</Link></li>
-        <li><Link to="/projects">Projets</Link></li>
+      {/* Icône du menu burger (visible uniquement sur mobile) */}
+      <div className="burger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </div>
+
+      {/* Liste des liens */}
+      <ul className={menuOpen ? "nav-links open" : "nav-links"}>
+        <li><Link to="/" onClick={() => setMenuOpen(false)}>Accueil</Link></li>
+        <li><Link to="/profile" onClick={() => setMenuOpen(false)}>Mon Profil</Link></li>
+        <li><Link to="/veille" onClick={() => setMenuOpen(false)}>Veille Technologique</Link></li>
+        <li><Link to="/projects" onClick={() => setMenuOpen(false)}>Projets</Link></li>
       </ul>
     </nav>
   );
